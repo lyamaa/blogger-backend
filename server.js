3f4f8468a31d534f76dce1229371a9bf9bf22050
surajmt8848 <suraj8848mt@gmail.com>
2020-12-02 10:41:52 +0545

    connect Atlas: mongodb

--- server.js
+++ server.js
@@ -1,36 +1,45 @@
-const express = require('express')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
-const cookieParser = require("cookie-parser")
-const cors = require('cors')
-require('dotenv').config()
-
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 // app
-const app = express()
+const app = express();
 
+// Database connection
+mongoose.connect(
+  process.env.DATABASE,
+  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+  (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Database connected ....");
+    }
+  }
+);
 
 // Middlewares
-app.use(morgan('dev'))
-app.use(bodyParser.json())
-app.use(cookieParser())
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 // cors
-if(process.env.NODE_ENV == 'development'){
-    app.use(cors({origin: `${process.env.CLIENT_URL}` }))
+if (process.env.NODE_ENV == "development") {
+  app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
 }
 
-
-
 // Routes
-app.get('/api', (req, res) => {
-    res.json({time: Date().toString()})
-}) 
-
+app.get("/api", (req, res) => {
+  res.json({ time: Date().toString() });
+});
 
 // port
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
