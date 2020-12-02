daaa828bb9962ae73d34cc235730fbddc81bdc4f
surajmt8848 <suraj8848mt@gmail.com>
2020-12-02 14:27:20 +0545

    initialized validator on auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,10 @@ const router = express.Router()
 const {time} = require('../controllers/blog')
 const {signup} = require('../controllers/auth')
 
+const {runValidation} = require('../helpers')
+const {userSignupValidator} = require('../helpers/auth')
 
-router.post('/signup', signup)
+router.post('/signup', userSignupValidator, runValidation, signup)
 
 
 module.exports = router
